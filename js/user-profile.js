d4d0e64689fb7e80b44d46b4bbb0803406a5add5
Kalindu <krashmina612@gmail.com>
2023-06-10 19:52:09 +0530

    Kalindu Version 8

---
+++ js/user-profile.js
@@ -0,0 +1,55 @@
+function editProfile(){
+
+    console.log("hi");
+
+    let uname = document.getElementById("uname");
+    let fname = document.getElementById("fname");
+    let lname = document.getElementById("lname");
+    let email = document.getElementById("email");
+    let mobile = document.getElementById("mobile");
+    let bio = document.getElementById("bio");
+    let submitBtn = document.getElementById("submit");
+
+    if(uname.style.display === "none"){
+
+        uname.style.display = "block";
+        fname.style.display = "block";
+        lname.style.display = "block";
+        email.style.display = "block";
+        mobile.style.display = "block";
+        bio.style.display = "block";
+        submitBtn.style.display = "inline-block";
+
+    }
+    else{
+
+        uname.style.display = "none";
+        fname.style.display = "none";
+        lname.style.display = "none";
+        email.style.display = "none";
+        mobile.style.display = "none";
+        bio.style.display = "none";
+        submitBtn.style.display = "none";
+    }
+    
+
+    
+}
+
+
+
+function checkPwd(){
+
+    const newPwd = document.getElementById("newpwd").value;
+    const confirmPwd = document.getElementById("confirmpwd").value;
+
+    if(newPwd === confirmPwd){
+
+        return true;
+    }
+    else{
+        alert("Your password does not match, please enter again")
+        return false;
+    }
+}
+
---
+++ php/edit-profile.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    echo $loggedUser = $_SESSION["loggedUser"];
+
+    require "config.php";
+
+    if(isset($_POST["submit"])){
+
+        $uname = $_POST["uname"];
+        $fname = $_POST["fname"];
+        $lname = $_POST["lname"];
+        $email = $_POST["email"];
+        $mobile = $_POST["mobile"];
+        $bio = $_POST["bio"];
+
+        $sql = "UPDATE user SET First_Name = '$fname', Last_Name = '$lname', Email = '$email', Mobile = '$mobile', Bio = '$bio', Username = '$uname' WHERE Email = '$loggedUser' ";
+
+        if($conn->query($sql)){
+
+            echo "<script>alert('Details successfully updated')</script>";
+            $_SESSION["loggedUser"] = $email;
+            header("location: user-profile.php");
+        }
+        else{
+            echo "<script>alert('Error')</script>";
+        }
+    }
+
+?>
